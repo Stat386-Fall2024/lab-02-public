284325ef65197042fb5c925d0be29e72028cbff9
Shannon Neeley Tass <42216898+esnt@users.noreply.github.com>
2024-01-18 10:41:45 -0700

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,40 @@
+# import necessary libraries 
+
+# 1
+def euclid_dist():
+    pass
+
+
+# 2
+def sum_ns():
+    pass
+
+
+# 3
+def perfect_square():
+    pass
+
+
+# 4
+def is_palindrome():
+    pass
+
+
+# 5
+def count_letter_types():
+    pass
+
+
+# 6
+def count_word_lengths():
+    pass
+
+
+# 7
+def n_list():
+    pass
+
+
+# 8
+def evens_mean_max():
+    pass
